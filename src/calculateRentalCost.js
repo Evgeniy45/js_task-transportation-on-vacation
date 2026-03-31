1c76beda91888de1fe739ee6b1ae433cc22e7800
Evgeniy45 <jekaolhovskii@gmail.com>
2026-03-31 15:43:14 +0300

    fix: rename constants for better clarity

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,18 +5,18 @@
  */
 function calculateRentalCost(days) {
   const DAILY_RENT_PRICE = 40;
-  const SHORT_TERM = 20;
+  const SHORT_TERM_DAYS = 20;
   const SHORT_TERM_DISCOUNT = 3;
-  const LONG_TERM = 7;
+  const LONG_TERM_DAYS = 7;
   const LONG_TERM_DISCOUNT = 50;
   const totalRentCount = days * DAILY_RENT_PRICE;
 
-  if (days >= LONG_TERM) {
+  if (days >= LONG_TERM_DAYS) {
     return totalRentCount - LONG_TERM_DISCOUNT;
   }
 
   if (days >= SHORT_TERM_DISCOUNT) {
-    return totalRentCount - SHORT_TERM;
+    return totalRentCount - SHORT_TERM_DAYS;
   }
 
   return totalRentCount;
